771e8623093aca34de4555480df6fa970c72cba2
Hernando Muñoz Jimenez <156966097+nandojmj@users.noreply.github.com>
2024-05-03 13:00:01 -0500

    Update Principal.java

--- src/main/java/com/aluracurso/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracurso/screenmatch/principal/Principal.java
@@ -17,7 +17,7 @@ public class Principal {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=4fc7c187";
+    private final String API_KEY = "&apikey=YOUR_API_KEY";
     private ConvierteDatos conversor = new ConvierteDatos();
     public void muestraElMenu(){
         System.out.println("Escribe el nombre de la serie que deseas buscar");
